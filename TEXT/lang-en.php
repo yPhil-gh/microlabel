13583024bce31e3c115629cece57e059c0845351
xaccrocheur <xaccrocheur@gmail.com>
2014-04-21 14:57:42 +0100

    Moved text bits in ./TEXT

---
+++ TEXT/lang-en.php
@@ -0,0 +1,47 @@
+<?php
+define('TAGS_YEAR', 'Year');
+define('TAGS_GENRE', 'Genre');
+define('TAGS_TRACK', 'Track');
+define('TAGS_SHARE', 'Share');
+define('TAGS_RECORD_LABEL', 'Record Label');
+define('TAGS_PLAYTIME', 'Play Time');
+define('TAGS_SIZE', 'Size');
+define('TAGS_BITRATE', 'Bitrate');
+define('TAGS_VIDEO_VIDEO', 'Video');
+define('TAGS_VIDEO_PREVIEW', 'Preview');
+define('TAGS_DOWNLOAD', 'Download');
+define('TXT_NODOWNLOAD', 'Download is disabled for this album by request of the artist');
+define('TXT_DOWNLOAD', 'Download');
+define('TXT_SHARE', 'Share');
+define('TXT_BUY', 'Buy');
+define('TXT_NEXT_ALBUM', 'Next album');
+define('TXT_PREVIOUS_ALBUM', 'Previous album');
+define('TXT_BACK_TO_INDEX', 'Back to album list');
+define('TXT_ENGLISH', 'English');
+define('TXT_FRENCH', 'Français');
+define('TXT_SPANISH', 'Español');
+define('TXT_CLICKTOPLAY', 'play');
+define('TXT_PAUSE', 'pause');
+define('TXT_CLICKTOWATCH', 'Watch');
+define('TXT_HELP', 'Help');
+define('TXT_CLOSEPLAYER', 'Close player');
+define('URL_CCLICENSE', 'http://creativecommons.org/licenses/by-sa/2.0/fr/deed.en_GB');
+define('TXT_LICENSE', 'License');
+define('TXT_BASELINE', 'A (common) swift past the moon');
+define('TXT_LABEL', 'Label');
+define('TXT_MORE_INFO', 'More info about');
+define('TXT_DEBUG_ERRORS', 'Error(s)');
+define('TXT_DEBUG_FILE_ERRORS', 'File errors');
+define('TXT_DEBUG_SHOW_ERRORS', 'Show errors');
+define('TXT_DEBUG_ERROR_FOUND', 'One error was found in your setup');
+define('TXT_DEBUG_ERRORS_FOUND', 'errors were found in your setup');
+define('TXT_DEBUG_ERRORS_FILE', 'The file');
+define('TXT_DEBUG_ERRORS_FILE_SHOULD_BE_RENAMED', 'should be renamed');
+define('TXT_DEBUG_HELP_TXT', '<p>MicroLabelis a <a href="http://en.wikipedia.org/wiki/Content_management_system">CMS</a> intended to present music.</p>
+<ul><li>Because now we know what happens when artists let others present, distribute and sell their work, MicroLabel attempt to provide a link, as direct as possible, from musicians to audience.</li></ul>
+
+<p style="margin:1em;text-align:center;color:red">
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</p>');
+
+define('TXT_NO_AUDIO_TXT', '<p>Apparently your browser does not support multimedia formats. You can not listen to the songs on the site, just download, which is not bad you say, but most graphic embellishments will be disabled, which does not do justice to the work of designers.</p><p>May I suggest a standard browser?</p>');
+?>
---
+++ TEXT/lang-fr.php
@@ -0,0 +1,52 @@
+<?php
+define('TAGS_YEAR', 'Année');
+define('TAGS_GENRE', 'Genre');
+define('TAGS_TRACK', 'Piste');
+define('TAGS_SHARE', 'Partage');
+define('TAGS_RECORD_LABEL', 'Label');
+define('TAGS_PLAYTIME', 'Durée');
+define('TAGS_SIZE', 'Taille');
+define('TAGS_BITRATE', 'Encodage');
+define('TAGS_VIDEO_VIDEO', 'Vidéo');
+define('TAGS_VIDEO_PREVIEW', 'Aperçu');
+define('TAGS_DOWNLOAD', 'Téléchargement');
+define('TXT_NODOWNLOAD', 'Le téléchargement de cet album est désactivé à la demande de l\'artiste');
+define('TXT_DOWNLOAD', 'Télécharger');
+define('TXT_SHARE', 'Partager');
+define('TXT_BUY', 'Acheter');
+define('TXT_NEXT_ALBUM', 'Album suivant');
+define('TXT_PREVIOUS_ALBUM', 'Album précédent');
+define('TXT_BACK_TO_INDEX', 'Retour à la liste des albums');
+define('TXT_ENGLISH', 'English');
+define('TXT_SPANISH', 'Español');
+define('TXT_FRENCH', 'Français');
+define('TXT_CLICKTOPLAY', 'écouter');
+define('TXT_PAUSE', 'pause');
+define('TXT_CLICKTOWATCH', 'Regarder');
+define('TXT_HELP', 'Aide');
+define('TXT_CLOSEPLAYER', 'Fermer le player');
+define('URL_CCLICENSE', 'http://creativecommons.org/licenses/by-sa/2.0/fr/deed.fr_CA');
+define('TXT_LICENSE', 'Licence');
+define('TXT_BASELINE', 'Un martinet passe devant la lune');
+define('TXT_LABEL', 'Label');
+define('TXT_MORE_INFO', 'Plus d\'info sur');
+define('TXT_DEBUG_ERRORS', 'Erreur(s)');
+define('TXT_DEBUG_FILE_ERRORS', 'Erreurs de fichier');
+define('TXT_DEBUG_SHOW_ERRORS', 'Voir les erreurs');
+define('TXT_DEBUG_ERROR_FOUND', 'Une erreur a été trouvée dans votre installation');
+define('TXT_DEBUG_ERRORS_FOUND', 'erreurs ont été trouvées dans votre installation');
+define('TXT_DEBUG_ERRORS_FILE', 'Le fichier');
+define('TXT_DEBUG_ERRORS_FILE_SHOULD_BE_RENAMED', 'devrait être renommé');
+define('TXT_DEBUG_HELP_TXT', '<p>MicroLabel est un <a href="http://fr.wikipedia.org/wiki/Syst%C3%A8me_de_gestion_de_contenu">CMS</a> conçu pour permettre aux musiciens de présenter, distribuer et même vendre leur musique, puisqu\'il semble qu\'ils sont les seuls à même de le faire correctement et simplement : Un script central et un répertoire de musique, des mp3 ou des ogg-vorbis (qui qui est infiniment préférable)</p>
+<ul>
+<li>MicroLabel scanne le répertoire à la recherche de musique, lit les <a href="http://fr.wikipedia.org/wiki/ID3">tags</a> des fichiers qu\'il trouve, et organise tout ça en ARTISTE / ALBUM / TRACK, sur deux pages : Un index qui liste tous les albums, et une page de "lecture" qui liste toutes les chansongs.</li>
+<li>Le player lit les chansons en séquence, de la première sélectionnée à la dernière de l\'album.</li>
+<li>Un back-office est disponible pour gèrer / tagger les fichiers en ligne.</li></ul>
+
+<p style="margin:1em;text-align:center;color:red">
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />Ces oeuvres sont licenciées sous <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+</p>');
+
+define('TXT_NO_AUDIO_TXT', 'Apparemment votre navigateur ne gère pas les formats multimedia. Vous ne pourrez pas écouter les chansons sur le site, seulement les télécharger, ce qui n"est déjà pas si mal me direz-vous, mais la plupart des embellissements graphiques seront désactivés, ce qui ne fait pas justice au travail des designers.</p><br /><p>Si je peux me permettre, vous vous faites du mal à utiliser des outils défaillants ; puis-je vous suggèrer un navigateur standard ?');
+
+?>
