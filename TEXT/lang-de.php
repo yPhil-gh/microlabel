aa72b8a4f89562e0c1880fa87b2d52bfc79740a5
xaccrocheur <xaccrocheur@gmail.com>
2014-04-23 12:12:05 +0100

    German translation (Thanks to Ffanci)

---
+++ TEXT/lang-de.php
@@ -0,0 +1,48 @@
+<?php
+define('TAGS_YEAR', 'Jahr');
+define('TAGS_GENRE', 'Genre');
+define('TAGS_TRACK', 'Lied');
+define('TAGS_SHARE', 'Teilen');
+define('TAGS_RECORD_LABEL', 'Label');
+define('TAGS_PLAYTIME', 'Spieldauer');
+define('TAGS_SIZE', 'Gr&ouml;&szlig;e');
+define('TAGS_BITRATE', 'Bitrate');
+define('TAGS_VIDEO_VIDEO', 'Video');
+define('TAGS_VIDEO_PREVIEW', 'Vorschau');
+define('TAGS_DOWNLOAD', 'Herunterladen');
+define('TXT_NODOWNLOAD', 'Auf wunsch des K&uuml;nstlers ist der Download f&uuml;r dieses Album nicht erlaubt');
+define('TXT_DOWNLOAD', 'Herunterladen');
+define('TXT_SHARE', 'Teilen');
+define('TXT_BUY', 'Kaufen');
+define('TXT_NEXT_ALBUM', 'N&auml;chstes Album');
+define('TXT_PREVIOUS_ALBUM', 'Vorheriges Album');
+define('TXT_BACK_TO_INDEX', 'Zur&uuml;ck zur Albenliste');
+define('TXT_ENGLISH', 'English');
+define('TXT_FRENCH', 'Français');
+define('TXT_SPANISH', 'Español');
+define('TXT_GERMAN', 'Deutsch');
+define('TXT_CLICKTOPLAY', 'Abspielen');
+define('TXT_PAUSE', 'Pause');
+define('TXT_CLICKTOWATCH', 'Ansehen');
+define('TXT_HELP', 'Hilfe');
+define('TXT_CLOSEPLAYER', 'Player schlie&szlig;en');
+define('URL_CCLICENSE', 'http://creativecommons.org/licenses/by-sa/2.0/fr/deed.de');
+define('TXT_LICENSE', 'Lizens');
+define('TXT_BASELINE', 'Ein Mauersegler zog am Mond vorbei');
+define('TXT_LABEL', 'Label');
+define('TXT_MORE_INFO', 'Mehr Infos &uuml;ber');
+define('TXT_DEBUG_ERRORS', 'Fehler');
+define('TXT_DEBUG_FILE_ERRORS', 'Dateifehler');
+define('TXT_DEBUG_SHOW_ERRORS', 'Anzeigefehler');
+define('TXT_DEBUG_ERROR_FOUND', 'Ein Fehler wurde in Deinem Setup gefunden');
+define('TXT_DEBUG_ERRORS_FOUND', 'Fehler wurden in Deinem Setup gefunden');
+define('TXT_DEBUG_ERRORS_FILE', 'Die Datei');
+define('TXT_DEBUG_ERRORS_FILE_SHOULD_BE_RENAMED', 'sollte umbenannt werden');
+define('TXT_DEBUG_HELP_TXT', '<p>MicroLabel ist ein <a href="http://de.wikipedia.org/w/index.php?title=Content-Management-System">CMS</a> um Musik zu pr&auml;sentieren.</p>
+<ul><li>Weil wir jetzt wissen, was passiert, wenn K&uuml;nstler andere ihre Musik pr&auml;sentieren, verteilen und verkaufen lassen, versucht MicroLabel eine m&ouml;glichst direkte Verbindung zwischen Musikern und H&ouml;rern zu schaffen.</li></ul>
+
+<p style="margin:1em;text-align:center;color:red">
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.de"><img alt="Creative Commons Lizens" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />Dieses Werk steht unter einer <a rel="Lizens" href="http://creativecommons.org/licenses/by-sa/3.0/deed.de">Creative Commons Attribution-ShareAlike 3.0 Unported Lizens</a>.</p>');
+
+define('TXT_NO_AUDIO_TXT', '<p>Anscheinend unterst&uuml;tzt Dein Browser keine Multimediaformate. Du kannst die Musik auf dieser Seite nicht h&ouml;ren; lade sie einfach herunter, was nicht schlecht ist (sagst Du), aber die meisten grafischen Zierden werden nicht dargestellt, was den Designer gegen&uuml;ber unfair ist.</p><p>D&uuml;rfte ich einen Standard Browser empfehlen?</p>');
+?>
