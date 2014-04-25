ee9ef48950fdcac2ed990b74513bea7773db1634
xaccrocheur <xaccrocheur@gmail.com>
2014-04-25 19:39:38 +0100

    Cool error page

--- index.php
+++ index.php
@@ -839,7 +839,7 @@ function index($dirList, $labelName) {
 echo '
 		<div id="horizon">
 			<div id="error">
-					<h1 class="headline">Uh-ho</h1>
+					<h1 id="error">Uh-ho</h1>
 Something wrong happenned. I think you just deleted your Music directory :(
 			</div>
 		</div>
