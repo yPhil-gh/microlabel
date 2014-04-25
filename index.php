333a27f2478df628c29546d9ee09b1d4a76e4b6b
xaccrocheur <xaccrocheur@gmail.com>
2014-04-25 20:20:00 +0100

    Cool error page even on query-string

--- index.php
+++ index.php
@@ -1124,6 +1124,7 @@ $directoryToScan = $meanPath;
 $directoryToScan = trim($directoryToScan, $slash);
 $fileList = getInfo($directoryToScan, 'musicFiles');
 
+
 // Main block
 if (!isset($_GET['a'])) {
     $thereIsMusic = index($dirList, $labelName, 'ask');
