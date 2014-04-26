469591a9f1b2f6d7c4baeb21d95988b751da24a8
xaccrocheur <xaccrocheur@gmail.com>
2014-04-26 14:41:41 +0100

    Error lib

---
+++ error.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+    <style type="text/css" media="screen">@import "../css/style.css";</style>
+    <script src="libs/jquery-1.5.1.min.js"></script>
+    <script>
+    $(document).ready(function () {
+        $('.microlabel-index').hide();
+    });
+    </script>
+
+    </head>
+
+<body class="microlabel-body">
+
+<?php
+
+require_once('index.php');
+
+microlabelError("Access denied");
+
+?>
+
+</body>
+</html>
